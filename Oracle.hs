e3e3370c89d900811339a10ac062ce3a4dc3fba6
Carlos Valiente <carlos@pepelabs.net>
2013-11-22 19:08:33 +0000

    An encryption oracle.

---
+++ Oracle.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Control.Monad.Identity (Identity, runIdentity)
+import Control.Monad.Reader (ReaderT, ask, runReaderT)
+
+type Key = String
+type Prefix = String
+type Suffix = String
+
+data OracleEnv = OracleEnv {
+      key :: Key
+    , prefix :: Prefix
+    , suffix :: Suffix
+}
+
+type Oracle a = ReaderT OracleEnv Identity a
+
+oracle    :: String -> Oracle String
+oracle bs = do
+  env <- ask
+  return $ "[encrypt(" ++ prefix env ++ bs ++ suffix env ++ ", " ++ key env ++ ")]"
+
+runOracle         :: Oracle a -> OracleEnv -> a
+runOracle ocl env = runIdentity (runReaderT ocl env)
+
+main :: IO ()
+main = do
+  let env = OracleEnv "YELLOW SUBMARINE" "AAAAAA" "BBBBBB"
+  putStrLn $ runOracle (oracle "Not yet") env
