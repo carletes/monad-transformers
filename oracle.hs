0d7f7a067cbacac0a7572821335670f2bbe0f420
Carlos Valiente <carlos@pepelabs.net>
2013-11-26 19:57:10 +0000

    Interactive command-line tool

--- oracle.hs
+++ oracle.hs
@@ -1,8 +1,18 @@
 module Main where
 
+import System.Exit (exitWith, ExitCode(..))
+
 import Oracle (OracleEnv(..), oracle, runOracle)
 
 main :: IO ()
 main = do
   let env = OracleEnv "YELLOW SUBMARINE" "AAAAAA"
-  putStrLn $ runOracle (oracle "Not yet") env
+      loop :: IO ()
+      loop = do
+         bs <- getLine
+         if bs == ""
+            then exitWith $ ExitSuccess
+         else do
+             putStrLn $ runOracle (oracle bs) env
+             loop
+  loop
